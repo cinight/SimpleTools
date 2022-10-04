1032e40c1e414697e7f0a0103024e7407bbdfd66
MingWai Chan <mingwai@unity3d.com>
2022-10-04 16:42:13 +0200

    adding shader stripping sample script

---
+++ Assets/Editor/StrippingExample_Shader.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+class StrippingExample_Shader : IPreprocessShaders
+{
+    public StrippingExample_Shader()
+    {
+
+    }
+
+    public int callbackOrder { get { return 0; } }
+    public void OnProcessShader(Shader shader, ShaderSnippetData snippet, IList<ShaderCompilerData> data)
+    {
+        for (int i = 0; i < data.Count; ++i)
+        {
+            //Get a string of keywords
+            string variantText = "";
+            foreach(ShaderKeyword s in data[i].shaderKeywordSet.GetShaderKeywords())
+            {
+                variantText += " " +s.name;
+            }
+
+            bool wantToStrip = false;
+
+            //Only stripping specific shader's specific keyword
+            if(
+                shader.name == "Test/MyShader" && variantText.Contains("_USELESSKEYWORD")
+            )
+            {
+                wantToStrip = true;
+            }
+
+            if ( wantToStrip )
+            {
+                //Strip the variant
+                data.RemoveAt(i);
+                --i;
+            }
+        }
+    }
+}
