e4d9def07ab206e8da6b2adc681a9d9b0eadcac8
MingWai Chan <mingwai@unity3d.com>
2024-01-05 15:38:44 +0100

    add compute shader stripping code

--- Assets/Editor/StrippingExample_Shader.cs
+++ Assets/Editor/StrippingExample_Shader.cs
@@ -42,3 +42,44 @@ class StrippingExample_Shader : IPreprocessShaders
         }
     }
 }
+
+//==============================
+
+class StrippingExample_ComputeShader : IPreprocessComputeShaders
+{
+    public StrippingExample_ComputeShader()
+    {
+
+    }
+
+    public int callbackOrder { get { return 0; } }
+    public void OnProcessComputeShader(ComputeShader shader, string kernelName, IList<ShaderCompilerData> data)
+    {
+        for (int i = 0; i < data.Count; ++i)
+        {
+            //Get a string of keywords
+            string variantText = "";
+            foreach(ShaderKeyword s in data[i].shaderKeywordSet.GetShaderKeywords())
+            {
+                variantText += " " +s.name;
+            }
+
+            bool wantToStrip = false;
+
+            //Only stripping specific shader's specific keyword
+            if(
+                shader.name == "Test/MyShader" && variantText.Contains("_USELESSKEYWORD")
+            )
+            {
+                wantToStrip = true;
+            }
+
+            if ( wantToStrip )
+            {
+                //Strip the variant
+                data.RemoveAt(i);
+                --i;
+            }
+        }
+    }
+}
